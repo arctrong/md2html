98f107cbd7c74fe845eca83f4df66618e3723c43
someone <someone@someplace.world>
2021-09-17 23:23:28 +0300

    internal batch processing added in Python version: first working version

--- python/md2html.py
+++ python/md2html.py
@@ -18,8 +18,10 @@ EXEC_NAME = 'md2html_py'
 EXEC_VERSION = '0.1.2'
 
 WORKING_DIR = Path(__file__).resolve().parent
-MARKDOWN_CONVERTER = markdown.Markdown(extensions=["extra", "toc", "mdx_emdash", 
+MARKDOWN_CONVERTER = markdown.Markdown(extensions=["extra", "toc", "mdx_emdash",
                                                    "pymdownx.superfences", "admonition"])
+
+CACHED_FILES = {}
 
 
 def read_lines_from_file(file):
@@ -27,8 +29,39 @@ def read_lines_from_file(file):
         return file_handler.read()
 
 
+def read_lines_from_cached_file(file):
+    lines = CACHED_FILES.get(file)
+    if lines is None:
+        lines = read_lines_from_file(file)
+        CACHED_FILES[file] = lines
+    return lines
+
+
+def read_lines_from_commented_file(file, comment_char='#'):
+    lines = []
+    with open(file, 'r') as file_handler:
+        for line in file_handler:
+            if line.strip().startswith(comment_char):
+                lines.append(re.sub(r'[^\r\n]', '', line))
+            else:
+                lines.append(line)
+    return ''.join([item for item in lines])
+
+
 def strip_extension(path):
     return os.path.splitext(path)[0]
+
+
+def first_not_none(*values):
+    return next((v for v in values if v is not None), None)
+
+
+def default_output_file(input_file):
+    return strip_extension(input_file) + '.html'
+
+
+def default_template():
+    return WORKING_DIR.joinpath(DEFAULT_TEMPLATE_PATH)
 
 
 def extract_metadata_section(text):
@@ -79,7 +112,7 @@ def parse_metadata(metadata_section):
 
 def parse_md2html_arguments(*args):
     """
-    Returns a tuple of (result_type, result), where result_type is one of the 'success', 'help' or `error`.
+    Returns a tuple of (result_type, result), where result_type is one of the 'success', 'help' or 'error'.
     If the result is not successful then all user information messages are already printed into console.
     """
 
@@ -93,6 +126,7 @@ def parse_md2html_arguments(*args):
                                      formatter_class=formatter_creator, add_help=False)
     parser.add_argument("-h", "--help", help="shows this help message and exits", action='store_true')
     parser.add_argument("-i", "--input", help="input Markdown file name (mandatory)", type=str)
+    parser.add_argument("--argument-file", help="argument file", type=str)
     parser.add_argument("-o", "--output", help="output HTML file name, defaults to input file name with '.html' "
                                                "extension", type=str)
     parser.add_argument("-t", "--title", help="the HTML page title, if omitted there will be an empty title", type=str)
@@ -113,35 +147,39 @@ def parse_md2html_arguments(*args):
         parser.print_help()
         return 'help', None
 
+    if args.argument_file:
+        # if args.input:
+        #     parser.print_usage()
+        #     print(f'--argument-file is not compatible with --input ({USE_HELP_TEXT})')
+        #     return 'error', None
+        # elif args.output:
+        #     parser.print_usage()
+        #     print(f'--argument-file is not compatible with --output ({USE_HELP_TEXT})')
+        #     return 'error', None
+        md2html_args['argument_file'] = Path(args.argument_file)
+
     if args.input:
-        md2html_args['input_file'] = Path(args.input)
-    else:
+        input_file = Path(args.input)
+        md2html_args['input_file'] = input_file
+    elif not args.argument_file:
         parser.print_usage()
         print(f'Input file is not specified ({USE_HELP_TEXT})')
         return 'error', None
 
-    if args.output:
-        md2html_args['output_file'] = Path(args.output)
-    else:
-        md2html_args['output_file'] = Path(strip_extension(md2html_args['input_file']) + '.html')
+    md2html_args['output_file'] = Path(args.output) if args.output else None
 
     md2html_args['title'] = args.title
 
-    md2html_args['template'] = Path(args.template) if args.template else \
-        WORKING_DIR.joinpath(DEFAULT_TEMPLATE_PATH)
+    md2html_args['template'] = Path(args.template) if args.template else None
 
+    md2html_args['no_css'] = True if args.no_css else False
     if args.no_css and (args.link_css or args.include_css):
         parser.print_usage()
         print(f'--no-css argument is not compatible with --link-css and --include-css arguments ({USE_HELP_TEXT})')
         return 'error', None
 
-    md2html_args['link_css'] = args.link_css
-    if args.include_css:
-        md2html_args['include_css'] = [Path(item) for item in args.include_css]
-    elif args.no_css or md2html_args['link_css']:
-        md2html_args['include_css'] = None
-    else:
-        md2html_args['include_css'] = [WORKING_DIR.joinpath(DEFAULT_CSS_FILE_PATH)]
+    md2html_args['link_css'] = args.link_css if args.link_css else []
+    md2html_args['include_css'] = [Path(item) for item in args.include_css] if args.include_css else []
 
     md2html_args['force'] = args.force
     md2html_args['verbose'] = args.verbose
@@ -174,8 +212,7 @@ def md2html(**kwargs):
                 print(f'The output file is up-to-date. Skipping: {output_file}')
             return
 
-    with open(input_file, 'r') as md_file:
-        md_lines = md_file.read()
+    md_lines = read_lines_from_file(input_file)
 
     substitutions = {'title': title}
 
@@ -216,7 +253,10 @@ def md2html(**kwargs):
     substitutions['generation_date'] = current_time.strftime('%Y-%m-%d')
     substitutions['generation_time'] = current_time.strftime('%H:%M:%S')
 
-    template = Template(read_lines_from_file(template_file))
+    template = Template(read_lines_from_cached_file(template_file))
+
+    # template = Template(read_lines_from_file(template_file))
+
     result = template.safe_substitute(substitutions)
 
     with open(output_file, 'w') as result_file:
@@ -228,11 +268,162 @@ def md2html(**kwargs):
         print(output_file)
 
 
+def parse_argument_file(argument_string, cli_args):
+    """
+    Returns a tuple (success, error_message, document_list), where success is ether `True` or `False`.
+    """
+
+    arg_file_default_item = 'default'
+    arg_file_documents_item = 'documents'
+
+    # cli_input_file = cli_args.get('input_file')
+    # cli_output_file = cli_args.get('output_file')
+    # cli_title = cli_args.get('title')
+    # cli_template_file = cli_args.get('template')
+    # cli_link_css = cli_args.get('link_css')
+    # cli_include_css = cli_args.get('include_css')
+    # cli_no_css = cli_args.get('no_css')
+    # cli_force = cli_args.get('force')
+    # cli_verbose = cli_args.get('verbose')
+    # cli_report = cli_args.get('report')
+
+    document_list = []
+
+    try:
+        arguments_item = json.loads(argument_string)
+    except Exception as e:
+        return False, f'Argument file cannot be parsed: {type(e).__name__}: {e}', None
+
+    if not isinstance(arguments_item, dict):
+        return False, f"The root element is of type '{type(arguments_item).__name__}', not an object.", None
+
+    if arg_file_default_item in arguments_item:
+        defaults_item = arguments_item.get(arg_file_default_item)
+        if not isinstance(defaults_item, dict):
+            return False, f"'default' section is of type '{type(defaults_item).__name__}', not an object.", None
+    else:
+        defaults_item = {}
+
+    documents_item = arguments_item.get(arg_file_documents_item)
+    if documents_item is None:
+        return False, f"'documents' section is absent.", None
+    elif not isinstance(documents_item, list):
+        return False, f"'documents' section is of type '{type(documents_item).__name__}', not a list.", None
+
+    if 'no-css' in defaults_item and ('link-css' in defaults_item or 'include-css' in defaults_item):
+        return (False, f"'no-css' parameter incompatible with one of the ['link-css', 'include-css'] "
+                       f"in the 'default' section.", None)
+
+    for document_item in documents_item:
+        document = {}
+
+        v = first_not_none(cli_args.get('input_file'), document_item.get("input"), defaults_item.get("input"))
+        if v is not None:
+            document['input_file'] = Path(v)
+        else:
+            return False, f"Undefined input file for 'documents' item: {document_item}.", None
+
+        v = first_not_none(cli_args.get('output_file'), document_item.get("output"), defaults_item.get("output"))
+        document['output_file'] = Path(v) if v is not None else None
+
+        attr = 'title'
+        document[attr] = first_not_none(cli_args.get(attr), document_item.get(attr), defaults_item.get(attr))
+
+        attr = 'template'
+        v = first_not_none(cli_args.get(attr), document_item.get(attr), defaults_item.get(attr))
+        document[attr] = Path(v) if v is not None else None
+
+        # if 'no-css' in defaults_item:
+        #     return False, "'no-css' parameter cannot be set in the 'default' section. It's implied anyway.", None
+        link_css = []
+        include_css = []
+        no_css = False
+        if cli_args.get('no_css') or cli_args.get('link_css') or cli_args.get('include_css'):
+            if cli_args.get('no_css'):
+                no_css = True
+            else:
+                link_css.extend(first_not_none(cli_args.get('link_css'), []))
+                include_css.extend(first_not_none(cli_args.get('include_css'), []))
+        else:
+            link_args = ["link-css", "add-link-css", "include-css", "add-include-css"]
+            if 'no-css' in document_item and any(document_item.get(k) for k in link_args):
+                q = '\''
+                return (False, f"'no-css' parameter incompatible with one of "
+                               f"[{', '.join([q + a + q for a in link_args])}] "
+                               f"in `documents` item: {document_item}.", None)
+
+            no_css = first_not_none(document_item.get('no-css'), defaults_item.get('no-css'), False)
+
+            link_css.extend(first_not_none(document_item.get('link-css'), defaults_item.get('link-css'), []))
+            link_css.extend(first_not_none(defaults_item.get('add-link-css'), []))
+            include_css.extend(first_not_none(document_item.get('include-css'),
+                                              defaults_item.get('include-css'), []))
+            include_css.extend(first_not_none(defaults_item.get('add-include-css'), []))
+
+            if link_css or include_css:
+                no_css = False
+
+        document['link_css'] = link_css
+        document['include_css'] = include_css
+        document['no_css'] = no_css
+
+        # print(document['link_css'])
+        # print(document['include_css'])
+        # print(document['no_css'])
+
+        attr = 'force'
+        document[attr] = first_not_none(True if cli_args.get(attr) else None,
+                                        document_item.get(attr), defaults_item.get(attr), False)
+        attr = 'verbose'
+        document[attr] = first_not_none(True if cli_args.get(attr) else None,
+                                        document_item.get(attr), defaults_item.get(attr), False)
+        attr = 'report'
+        document[attr] = first_not_none(True if cli_args.get(attr) else None,
+                                        document_item.get(attr), defaults_item.get(attr), False)
+
+        if document['report'] and document['verbose']:
+            return False, f"Incompatible 'report' and 'verbose' parameters for 'documents' item: {document_item}.", None
+
+        document_list.append(document)
+
+    return True, None, document_list
+
+
+def enrich_document_list(document_list):
+    for document in document_list:
+        if not document['template']:
+            document['template'] = default_template()
+        if not document['output_file']:
+            document['output_file'] = Path(default_output_file(document['input_file']))
+        if not document['no_css'] and not document['link_css'] and not document['include_css']:
+            document['include_css'] = [WORKING_DIR.joinpath(DEFAULT_CSS_FILE_PATH)]
+
+
 def main():
     result_type, md2html_args = parse_md2html_arguments(sys.argv[1:])
     if result_type != 'success':
         sys.exit(1)
-    md2html(**md2html_args)
+
+    argument_file = md2html_args.get('argument_file')
+    if argument_file:
+        argument_file_string = read_lines_from_commented_file(argument_file)
+        success, error_message, document_list = parse_argument_file(argument_file_string, md2html_args)
+        if not success:
+            print(f"Error parsing argument file '{argument_file}': " + error_message)
+            sys.exit(1)
+    else:
+        document_list = [md2html_args]
+
+    enrich_document_list(document_list)
+
+    # for doc in document_list:
+    #     print(doc)
+    # if argument_file:
+    #     print("--argument_file option is now under construction")
+    #     sys.exit(1)
+
+    for document in document_list:
+        md2html(**document)
 
 
 if __name__ == '__main__':
