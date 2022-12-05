8b54ee3da1e76361f52380d518dce5d47ba0ee56
someone <someone@someplace.world>
2022-12-05 19:24:56 +0100

    sample project creation script (Windows only)

---
+++ bin/new_project/new_project.bat
@@ -0,0 +1,29 @@
+@echo off
+
+set CANNOT_PROCEED=
+
+call :check_file_or_dir doc_src
+call :check_file_or_dir doc
+call :check_file_or_dir md2html_args.json
+call :check_file_or_dir generate_doc.bat
+
+if [%CANNOT_PROCEED%]==[Y] (
+    echo Some problems found (see above^). Nothing was done
+    exit /b
+)
+
+xcopy %MD2HTML_HOME%\bin\new_project\doc_src doc_src\ /e
+xcopy %MD2HTML_HOME%\bin\new_project\doc doc\ /e
+xcopy %MD2HTML_HOME%\doc\layout doc\layout\ /e
+xcopy %MD2HTML_HOME%\bin\new_project\md2html_args.json
+xcopy %MD2HTML_HOME%\bin\new_project\readme.txt
+xcopy %MD2HTML_HOME%\generate_doc.bat
+
+exit /b
+
+:check_file_or_dir
+if exist %1 (
+    echo File or directory '%1' already exists
+    set CANNOT_PROCEED=Y
+)
+exit /b
---
+++ bin/new_project/readme.txt
@@ -0,0 +1,26 @@
+<!--VARIABLES {"noPageTitle": true}-->
+
+<p><img src="doc/pict/sample_picture.jpg" title="md2html" style="float: right; margin: 0 0 25px 25px;" /></p>
+
+<!--index Home page -->
+This is an automatically created documentation template.
+
+This home page uses a metadata variable to remove the page title.
+
+[TOC]
+
+<p style="clear: both;"></p>
+
+
+# What does it contain?
+
+This sample documentation contains several simple pages rendered using a typical template.
+
+
+<!--index usage -->
+# How to use it?
+
+- Add your content.
+- Remove unnecessary elements.
+- Redefine the settings if required.
+
