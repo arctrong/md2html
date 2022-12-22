e7860542fcabc55a5fb4c0b9c9dd4e78544a5abe
someone <someone@someplace.world>
2022-12-22 21:51:49 +0100

    Java: relative paths substitution added

--- java/src/main/java/world/md2html/plugins/RelativePathsPlugin.java
+++ java/src/main/java/world/md2html/plugins/RelativePathsPlugin.java
@@ -1,42 +1,63 @@
 package world.md2html.plugins;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import world.md2html.options.argfile.ArgFileParseException;
 import world.md2html.options.model.Document;
 import world.md2html.utils.CheckedIllegalArgumentException;
 import world.md2html.utils.UserError;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static world.md2html.utils.Utils.relativizeRelativePath;
 
-public class RelativePathsPlugin extends AbstractMd2HtmlPlugin {
+public class RelativePathsPlugin extends AbstractMd2HtmlPlugin implements PageMetadataHandler {
 
-    // TODO Consider extending this plugin for substitution of the paths inside documents.
-    //  Like what `PageLinksPlugin` does. Probably need to add parameter `marker` the the
-    //  `path` definition.
-
-    private Map<String, String> data = null;
+    private Map<String, String> paths;
+    private List<String> markers;
+    private List<PageMetadataHandlerInfo> handlers;
 
     @Override
     public void acceptData(JsonNode data) throws ArgFileParseException {
         validateInputDataAgainstSchemaFromResource(data, "plugins/relative_paths_schema.json");
+
+        JsonNode pathsNode;
+        JsonNode markers = data.get("markers");
+        if (markers instanceof ArrayNode) {
+            List<String> markerList = new ArrayList<>();
+            markers.forEach(node -> markerList.add(node.asText()));
+            this.markers = markerList;
+            pathsNode = data.get("paths");
+        } else {
+            this.markers = Collections.emptyList();
+            pathsNode = data;
+        }
+
         Map<String, String> pluginData = new HashMap<>();
-        data.fields().forEachRemaining(entry -> pluginData.put(entry.getKey(),
+        pathsNode.fields().forEachRemaining(entry -> pluginData.put(entry.getKey(),
                 entry.getValue().asText()));
-        this.data = pluginData;
+        this.paths = pluginData;
+
+        this.handlers = this.markers.stream()
+                .map(m -> new PageMetadataHandlerInfo(this, m, false))
+                .collect(Collectors.toList());
     }
 
     @Override
     public boolean isBlank() {
-        return this.data.isEmpty();
+        return this.paths.isEmpty();
+    }
+
+    @Override
+    public List<PageMetadataHandlerInfo> pageMetadataHandlers() {
+        return this.handlers;
     }
 
     @Override
     public Map<String, Object> variables(Document document) {
         Map<String, Object> variables = new HashMap<>();
-        this.data.forEach((k, v) -> {
+        this.paths.forEach((k, v) -> {
             try {
                 variables.put(k, relativizeRelativePath(v, document.getOutput()));
             } catch (CheckedIllegalArgumentException e) {
@@ -47,4 +68,21 @@ public class RelativePathsPlugin extends AbstractMd2HtmlPlugin {
         return variables;
     }
 
+    @Override
+    public String acceptPageMetadata(Document document, String marker, String metadata,
+            String metadataSection) throws PageMetadataException {
+
+        String path = this.paths.get(metadata.trim());
+        if (path == null) {
+            return metadataSection;
+        } else {
+            try {
+                return relativizeRelativePath(path, document.getOutput());
+            } catch (CheckedIllegalArgumentException e) {
+                throw new PageMetadataException("Plugin '" + this.getClass().getSimpleName() +
+                        "': Cannot relativize '" + path + "' against '" +
+                        document.getOutput() + "': " + e.getMessage());
+            }
+        }
+    }
 }
--- java/src/test/java/world/md2html/plugins/RelativePathsPluginTest.java
+++ java/src/test/java/world/md2html/plugins/RelativePathsPluginTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import world.md2html.options.argfile.ArgFileParseException;
 import world.md2html.options.model.ArgFile;
 import world.md2html.options.model.CliOptions;
+import world.md2html.options.model.Document;
+import world.md2html.pagemetadata.PageMetadataHandlersWrapper;
 import world.md2html.testutils.PluginTestUtils;
 
 import java.util.List;
@@ -23,11 +25,33 @@ class RelativePathsPluginTest {
     }
 
     @Test
-    public void notActivated() throws ArgFileParseException {
+    public void notDefined() throws ArgFileParseException {
         ArgFile argFile = parseArgumentFile("{\"documents\": " +
                 "[{\"input\": \"whatever.md\"}], \"plugins\": {}}", DUMMY_CLI_OPTIONS);
         RelativePathsPlugin plugin = findSinglePlugin(argFile.getPlugins());
         assertNull(plugin);
+    }
+
+    @Test
+    public void notActivated_newSyntax() throws ArgFileParseException {
+        ArgFile argFile = parseArgumentFile("{\"documents\": [{\"input\": \"whatever.md\"}], " +
+                "\"plugins\": {" +
+                "    \"relative-paths\": {\"markers\": [\"p1\", \"p2\"], \"paths\": {}}" +
+                "}}", DUMMY_CLI_OPTIONS);
+        RelativePathsPlugin plugin = findSinglePlugin(argFile.getPlugins());
+        assertNull(plugin);
+    }
+
+    @Test
+    public void minimal_newSyntax() throws ArgFileParseException {
+        ArgFile argFile = parseArgumentFile("{\"documents\": [{\"input\": \"whatever.md\"}], " +
+                "\"plugins\": {" +
+                "    \"relative-paths\": {\"markers\": [\"path\"], " +
+                "        \"paths\": {\"pict\": \"doc/pict/\"}}" +
+                "}}", DUMMY_CLI_OPTIONS);
+        RelativePathsPlugin plugin = findSinglePlugin(argFile.getPlugins());
+        Map<String, Object> relPaths = plugin.variables(documentWithOutputLocation("root.html"));
+        assertEquals("doc/pict/", relPaths.get("pict"));
     }
 
     @Test
@@ -67,4 +91,87 @@ class RelativePathsPluginTest {
         assertEquals("../../../../", relPaths.get("up2"));
     }
 
+    @Test
+    public void substitution_minimal() throws ArgFileParseException {
+        ArgFile argFile = parseArgumentFile("{\"documents\": [{\"input\": \"page1.txt\"}], \n" +
+                "\"plugins\": { \n" +
+                "    \"relative-paths\": {\"markers\": [\"path1\"], " +
+                "        \"paths\": {\"pict1\": \"doc/pict/\"}} \n" +
+                "}}", DUMMY_CLI_OPTIONS);
+        Document doc = argFile.getDocuments().get(0);
+        PageMetadataHandlersWrapper metadataHandlers =
+                PageMetadataHandlersWrapper.fromPlugins(argFile.getPlugins());
+
+        String pageText = "![](<!--path1 pict1-->img1.png)";
+        String processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc);
+        assertEquals("![](doc/pict/img1.png)", processedPage);
+    }
+
+    @Test
+    public void different_paths() throws ArgFileParseException {
+        ArgFile argFile = parseArgumentFile("{\"documents\": [ \n" +
+                "    {\"input\": \"page1.txt\"}, \n" +
+                "    {\"input\": \"subdir/page2.txt\"} \n" +
+                "], \n" +
+                "\"plugins\": { \n" +
+                "    \"relative-paths\": {\"markers\": [\"path2\"], " +
+                "        \"paths\": {\"pict2\": \"doc/pict/\"}} \n" +
+                "}}", DUMMY_CLI_OPTIONS);
+        Document doc1 = argFile.getDocuments().get(0);
+        Document doc2 = argFile.getDocuments().get(1);
+        PageMetadataHandlersWrapper metadataHandlers =
+                PageMetadataHandlersWrapper.fromPlugins(argFile.getPlugins());
+
+        String pageText = "![](<!--path2 pict2-->img2.png)";
+        String processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc1);
+        assertEquals("![](doc/pict/img2.png)", processedPage);
+
+        processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc2);
+        assertEquals("![](../doc/pict/img2.png)", processedPage);
+    }
+
+    @Test
+    public void unknown_path_must_ignore() throws ArgFileParseException {
+        ArgFile argFile = parseArgumentFile("{\"documents\": [{\"input\": \"page1.txt\"}], \n" +
+                "\"plugins\": { \n" +
+                "    \"relative-paths\": {\"markers\": [\"path2\"], " +
+                "        \"paths\": {\"pict2\": \"doc/pict/\"}} \n" +
+                "}}", DUMMY_CLI_OPTIONS);
+        Document doc1 = argFile.getDocuments().get(0);
+        PageMetadataHandlersWrapper metadataHandlers =
+                PageMetadataHandlersWrapper.fromPlugins(argFile.getPlugins());
+
+        String pageText = "![](<!--path2 unknown-->img2.png)";
+        String processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc1);
+        assertEquals("![](<!--path2 unknown-->img2.png)", processedPage);
+    }
+
+    @Test
+    public void several_markers() throws ArgFileParseException {
+        ArgFile argFile = parseArgumentFile("{\"documents\": [{\"input\": \"page1.txt\"}], \n" +
+                "\"plugins\": { \n" +
+                "    \"relative-paths\": {\"markers\": [\"p1\", \"p2\"], \n" +
+                "        \"paths\": {\"pict2\": \"doc/pict/\", " +
+                "                    \"pict3\": \"doc/layout/pict/\"}} \n" +
+                "}}", DUMMY_CLI_OPTIONS);
+        Document doc1 = argFile.getDocuments().get(0);
+        PageMetadataHandlersWrapper metadataHandlers =
+                PageMetadataHandlersWrapper.fromPlugins(argFile.getPlugins());
+
+        String pageText = "![](<!--p1 pict2-->img.png)";
+        String processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc1);
+        assertEquals("![](doc/pict/img.png)", processedPage);
+
+        pageText = "![](<!--p2 pict2-->img.png)";
+        processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc1);
+        assertEquals("![](doc/pict/img.png)", processedPage);
+
+        pageText = "![](<!--p1 pict3-->img.png)";
+        processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc1);
+        assertEquals("![](doc/layout/pict/img.png)", processedPage);
+
+        pageText = "![](<!--p2 pict3-->img.png)";
+        processedPage = metadataHandlers.applyMetadataHandlers(pageText, doc1);
+        assertEquals("![](doc/layout/pict/img.png)", processedPage);
+    }
 }
