c3d9d651a3f8871b1a8addd61c050e1e739bf760
arctrong <arctrong@luner.u>
2023-07-16 00:14:55 +0200

    Windows context menu scripts updated

--- bin/context_menu/md2html_prompt.bat
+++ bin/context_menu/md2html_prompt.bat
@@ -104,7 +104,7 @@ goto again
 
 :continue
 for /f "delims=" %%a in ( '%~dp0..\_set_executable.bat %IMPL% %*' ) do set EXEC=%%a
-call %EXEC% %FORCE% %TITLE% -v
+call %EXEC% %FORCE% %TITLE% -v "--include-css=%MD2HTML_HOME%\doc\layout\styles.css" "--include-css=%MD2HTML_HOME%\doc\themes\light_default_content.css"
 echo.
 pause
 
