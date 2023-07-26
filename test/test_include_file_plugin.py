8afe458ed90ab63a74d7ded27385189d288216ad
arctrong <arctrong@luner.u>
2023-07-26 12:50:31 +0200

    include-file plugin integral tests added

---
+++ test/test_include_file_plugin.py
@@ -0,0 +1,44 @@
+import unittest
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+import helpers as h
+
+
+class Md2htmlIncludeFilePluginIntegralTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.OUTPUT_DIR = h.prepare_output_directory(cls.__name__)
+        cls.INPUT_DIR = Path(h.INPUT_DIR).joinpath('IncludeFilePluginTest')
+        cls.COMMON_PARAMS = ['--input-root', str(cls.INPUT_DIR), '--output-root', cls.OUTPUT_DIR]
+
+    def test_simple(self):
+
+        h.run_with_parameters(
+            self.COMMON_PARAMS +
+            ['-f', '--argument-file', str(self.INPUT_DIR.joinpath('md2html_args.json'))]
+        )
+
+        with open(Path(self.OUTPUT_DIR).joinpath('page1.html')) as html_file:
+            root = BeautifulSoup(html_file, 'html.parser')
+
+        self.assertEqual('IncludeFilePlugin test', root.head.title.text)
+        h1 = root.body.h1
+        self.assertEqual('IncludeFilePlugin test', h1.text)
+
+        p = h1.find_next_sibling('p')
+        self.assertEqual('Include Java: [some Java code]', p.text)
+
+        p = p.find_next_sibling('p')
+        self.assertEqual('Include Shell: [\nsome Shell script\n]', p.text)
+
+        p = p.find_next_sibling('p')
+        self.assertEqual('Include Code 1: [some source code]', p.text)
+
+        p = p.find_next_sibling('p')
+        self.assertEqual('Include Code 2: [some source code]', p.text)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_input/IncludeFilePluginTest/code/java/source2.java
@@ -0,0 +1,2 @@
+
+some Java code
