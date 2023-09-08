ab51919aab5dea675b2cc906f602c840cbbbd020
arctrong <arctrong@luner.u>
2023-09-08 23:35:08 +0200

    responsive HTML design implemented: context menu script

--- bin/context_menu/md2html_prompt_fast.bat
+++ bin/context_menu/md2html_prompt_fast.bat
@@ -10,4 +10,4 @@ echo Converting the following file to HTML: %INPUT_FILE%
 echo.
 
 for /f "delims=" %%a in ( '%~dp0..\_set_executable.bat %IMPL% %*' ) do set EXEC=%%a
-call %EXEC% -fv "--include-css=%MD2HTML_HOME%\doc\layout\styles.css" "--include-css=%MD2HTML_HOME%\doc\themes\light_default_content.css" || pause 
+call %EXEC% -fv "--include-css=%MD2HTML_HOME%\doc\layout\content.css" "--include-css=%MD2HTML_HOME%\doc\themes\light\content_theme.css" || pause 
