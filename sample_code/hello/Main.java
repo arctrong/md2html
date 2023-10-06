bfbcb0a87e361c01f87326ba69623cdbf922287c
arctrong <arctrong@luner.u>
2023-10-06 15:36:39 +0200

    include_file plugin: per file substringing added: documentation updated

--- sample_code/hello/Main.java
+++ sample_code/hello/Main.java
@@ -1,7 +1,9 @@
 // This is an example of Java code.
 // INCLUDE FROM HERE
 public class Main {
+    // method start
     public static void main(String[] args) {
         System.out.println("Hi!");
     }
+    // method end
 }
